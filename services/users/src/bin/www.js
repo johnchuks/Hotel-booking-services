457c368cfb29a927e3ed5aabd059a63284f09704
Johnbosco Ohia <johnboscoohia@gmail.com>
2019-08-17 16:01:05 +0100

    setup challenge and user services

---
+++ services/users/src/bin/www.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+require('dotenv/config');
+require("@babel/polyfill");
+
+const app = express()
+const port = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(bodyparser());
+app.use(morgan('dev'))
+app.get('/', (req, res) => res.send('Weclome to JLBooking-user-service'))
+
+app.listen(port, () => {
+  console.log(`JLBooking-user-service listening on port ${port}!`)
+})
